700a7df264022fed461a2d1e76b3174bb8e1a6bd
awsh <scottshaw00@gmail.com>
2024-02-13 23:11:00 -0500

    added arch install

--- extensions.sh
+++ extensions.sh
@@ -3,6 +3,8 @@
 # install gnome extensions
 gnome-shell-extension-installer 750 
 gnome-shell-extension-installer 744
+gnome-shell-extension-installer 4099
 gnome-extensions enable openweather-extension@jenslody.de
 gnome-extensions enable Hide_Activities@shay.shayel.org
+gnome-extensions enable 4099
 
--- install.sh
+++ install.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
 
-# add repositories
-sudo add-apt-repository -y ppa:papirus/papirus
-sudo add-apt-repository -y multiverse
-sudo add-apt-repository -y universe
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
+# pacman
+sudo pacman -S $(cat pacman.txt) --noconfirm
 
-# apt
-sudo apt install -y $(cat apt_list.txt)
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
-# chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y ./google-chrome-stable_current_amd64.deb
-rm ./google-chrome-stable_current_amd64.deb
+yay -S rofi-lbonn-wayland-git
 
 # snaps
-sudo snap install alacritty --edge --classic
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
 sudo snap install spotify 
 sudo snap install whatsapp-for-linux
 sudo snap install discord
-
-# remove dock
-sudo apt remove -y gnome-shell-extension-ubuntu-dock
+sudo snap install steam
 
 wget -O gnome-shell-extension-installer https://github.com/brunelli/gnome-shell-extension-installer/raw/master/gnome-shell-extension-installer
 chmod +x gnome-shell-extension-installer
 sudo mv gnome-shell-extension-installer /usr/bin/
-
